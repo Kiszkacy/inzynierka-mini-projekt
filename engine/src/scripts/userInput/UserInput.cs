1f5bf5a0596d81eea54bb66745c7b4aabe4370c7
Kiszkacy <karolkisz22@gmail.com>
2024-04-15 22:50:20 +0200

    add simple userInput class

---
+++ engine/src/scripts/userInput/UserInput.cs
@@ -0,0 +1,61 @@
+using Godot;
+using System;
+
+public partial class UserInput : Node
+{
+	private Paddle leftPaddle;
+	private Paddle rightPaddle;
+	
+	public override void _Ready()
+	{
+		this.leftPaddle = GetNode<Paddle>("LeftPaddle");
+		this.rightPaddle = GetNode<Paddle>("RightPaddle");
+	}
+	
+	public override void _Process(double delta)
+	{
+		
+	}
+	
+	public override void _Input(InputEvent @event)
+	{
+		if (@event.IsActionPressed("left-up"))
+		{
+			this.leftPaddle.GoUp();
+		}
+		else if (@event.IsActionReleased("left-up"))
+		{
+			this.leftPaddle.StopUp();
+		}
+		else if (@event.IsActionPressed("left-down"))
+		{
+			this.leftPaddle.GoDown();
+		}
+		else if (@event.IsActionReleased("left-down"))
+		{
+			this.leftPaddle.StopDown();
+		}
+		
+		else if (@event.IsActionReleased("right-up"))
+		{
+			this.rightPaddle.GoUp();
+		}
+		else if (@event.IsActionReleased("right-up"))
+		{
+			this.rightPaddle.StopUp();
+		} 
+		else if (@event.IsActionPressed("right-down"))
+		{
+			this.rightPaddle.GoDown();
+		}
+		else if (@event.IsActionReleased("right-down"))
+		{
+			this.rightPaddle.StopDown();
+		}
+	}
+
+	public UserInput()
+	{
+		
+	}
+}
