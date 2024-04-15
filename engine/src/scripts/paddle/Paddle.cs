5294ddbf7c559513c42865658aae8dca7180e429
Kiszkacy <karolkisz22@gmail.com>
2024-04-15 22:43:38 +0200

    add very primitive paddle class

---
+++ engine/src/scripts/paddle/Paddle.cs
@@ -0,0 +1,59 @@
+﻿using Godot;
+using System;
+
+public partial class Paddle : Node2D
+{
+	private double speed = 5.0;
+	private bool movingUp;
+	private bool movingDown;
+	
+	[Export]
+	public double Speed
+	{
+		get => this.speed;
+		set => this.speed = value;
+	}
+    public override void _Ready()
+    {
+        
+    }
+
+    private void MovementProcess(double delta)
+    {
+	    Vector2 direction = Vector2.Zero;
+	    direction += this.movingUp ? Vector2.Up : Vector2.Zero; 
+	    direction += this.movingDown ? Vector2.Down : Vector2.Zero; 
+	    
+	    this.GlobalPosition += direction * (float)delta;
+    }
+	
+    public override void _Process(double delta)
+    {
+	    this.MovementProcess(delta);
+    }
+
+    public void GoUp()
+    {
+	    this.movingUp = true;
+    }
+    
+    public void StopUp()
+    {
+	    this.movingUp = false;
+    }
+    
+    public void GoDown()
+    {
+	    this.movingDown = true;
+    }
+    
+    public void StopDown()
+    {
+	    this.movingDown = false;
+    }
+
+    public Paddle()
+    {
+		
+    }
+}
