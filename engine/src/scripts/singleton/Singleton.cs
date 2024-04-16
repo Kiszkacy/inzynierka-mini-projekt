24686983931f0ed046ddefebb24437d4db9fa602
Kiszkacy <karolkisz22@gmail.com>
2024-04-16 17:15:46 +0200

    add Singleton class

---
+++ engine/src/scripts/singleton/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Singleton<T> where T : Singleton<T>
+{
+    // Lazy<T> should be thread safe !
+    private static readonly Lazy<T> instance = new(() =>
+    {
+        var createdInstance = Activator.CreateInstance(typeof(T), true);
+        return (T)createdInstance;
+    });
+    
+    public static T Instance => instance.Value;
+
+    public static T Get() => Instance;
+
+    protected Singleton() {}
+}
