d60ebefec908a82e73f419a58151a4fb3c6012a8
MasterHamster42 <115225781+MasterHamster42@users.noreply.github.com>
2024-04-26 01:31:49 +0200

    Replace print with logger

--- core/src/main.py
+++ core/src/main.py
@@ -1,12 +1,12 @@
 import torch
-
 from core.src.agents.pong_agent import PongAgent
 from core.src.environments.server_environment import ServerEnvironment
 from core.src.policies.pong_policy_network import PongPolicyNetwork
+from loguru import logger
 
 if __name__ == "__main__":
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"{DEVICE=}")
+    logger.info(f"Using {DEVICE=}")
 
     env = ServerEnvironment()
     policy = PongPolicyNetwork(6, 2)
