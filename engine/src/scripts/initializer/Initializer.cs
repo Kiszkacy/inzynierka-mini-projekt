d220b9f79a52c7e5710516faa5e1b8ab5df0c2cc
Kiszkacy <karolkisz22@gmail.com>
2024-04-30 16:31:19 +0200

    initializer cleanup

--- engine/src/scripts/initializer/Initializer.cs
+++ engine/src/scripts/initializer/Initializer.cs
@@ -4,9 +4,9 @@ using System;
 public partial class Initializer : Node
 {
 	public override void _Ready()
-	{	
-		Engine.TimeScale = 3.0;
-		Engine.PhysicsTicksPerSecond = 60;
+	{
+		this.LoadSingletons();
+		this.SetupEngineSettings();
 		GD.Print("OK!");
 	}
 	
@@ -19,6 +19,17 @@ public partial class Initializer : Node
 	{
 		// TODO this should be called in a special class that runs logic at the end of each godot frame
 		EventManager.Get().EmitDelayedEvents();
+	}
+
+	private void LoadSingletons() // this method loads singletons that are required to be loaded in a specific order
+	{
+		EventManager.Get();
+	}
+
+	private void SetupEngineSettings()
+	{
+		Engine.TimeScale = 3.0;
+		Engine.PhysicsTicksPerSecond = 60;
 	}
 
 	public Initializer()
