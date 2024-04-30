23f17ad26229c182484a33cef07365631db2811d
Kiszkacy <karolkisz22@gmail.com>
2024-04-30 17:15:35 +0200

    fix RewardHandler calculations

--- engine/src/scripts/rewardHandler/RewardHandler.cs
+++ engine/src/scripts/rewardHandler/RewardHandler.cs
@@ -1,41 +1,35 @@
 
 public class RewardHandler : Singleton<RewardHandler>, Observable
 {
-    private int rewardNow;
-    private int rewardBefore;
-
     private const int BounceReward = 1;
     private const int ScoreReward = 20;
     private const int EnemyScorePenalty = -20;
-    
-    public int Reward {
-        get {
-            int reward = this.rewardNow - this.rewardBefore;
-            this.rewardBefore = this.rewardNow;
-            return reward;
-        }
-    }
+    private int reward;
+    public int Reward => this.reward;
 
     public void Notify(Event @event)
-    {   
-        this.rewardBefore = this.rewardNow;
-
+    {
         if (@event.Code == "SIDE.RIGHT.BOUNCE")
         {
-            this.rewardNow += BounceReward;
+            this.reward += BounceReward;
         }
         else if (@event.Code == "SIDE.RIGHT.SCORE")
         {
-            this.rewardNow += ScoreReward;
+            this.reward += ScoreReward;
         }
         else if (@event.Code == "SIDE.LEFT.SCORE")
         {
-            this.rewardNow -= EnemyScorePenalty;
+            this.reward += EnemyScorePenalty;
         }
+    }
+
+    public void ResetReward()
+    {
+        this.reward = 0;
     }
 
     private RewardHandler()
 	{
-		
+		EventManager.Get().Subscribe(this);
 	}
 }
