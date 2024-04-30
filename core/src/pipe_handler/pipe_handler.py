f44b37ef2da3b576685842f1a9b125c993a0c69d
Kiszkacy <karolkisz22@gmail.com>
2024-04-30 13:23:59 +0200

    linter fixes

--- core/src/pipe_handler/pipe_handler.py
+++ core/src/pipe_handler/pipe_handler.py
@@ -8,7 +8,7 @@ READ_BUFFER_SIZE: int = 512
 
 
 class PipeHandler:
-    def __init__(self, pipe_name: str = r'\\.\pipe\godot-python-pipe') -> None:
+    def __init__(self, pipe_name: str = r"\\.\pipe\godot-python-pipe") -> None:
         self.pipe: int = win32pipe.CreateNamedPipe(
             pipe_name,
             win32pipe.PIPE_ACCESS_DUPLEX,
@@ -29,7 +29,7 @@ class PipeHandler:
     def send(self, data_bytes) -> None:
         win32file.WriteFile(self.pipe, data_bytes)
 
-    def receive(self) -> list[bytes]:
-        bytes_read: int; data: list[bytes]
+    def receive(self) -> bytes:
+        bytes_read: int; data: bytes
         bytes_read, data = win32file.ReadFile(self.pipe, READ_BUFFER_SIZE)
         return data
