d33e85a91b35438dbdd1388f06363c45986ae401
Kiszkacy <karolkisz22@gmail.com>
2024-05-14 16:34:38 +0200

    add an example test

---
+++ engine/tests/tests/simpleBot/TestSimpleBot.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Diagnostics;
+using Godot;
+
+public class TestSimpleBot : TestClass<TestSimpleBot>
+{
+    [Test]
+    public void GetAction_ShouldMoveDownWhenBallIsBelow()
+    {
+        // given
+        SimpleBot bot = MockSimpleBot.Get();
+        double paddlePosition = 0.0f;
+        Vector2 ballPosition = new Vector2(0.0f, 100.0f);
+        Vector2 ballVelocity = Vector2.Zero;
+
+        // when
+        Action action = bot.GetAction(paddlePosition: 0.0f, ballPosition: ballPosition, ballVelocity: ballVelocity);
+
+        // then
+        Debug.Assert(action == Action.Down); // TODO debug asserts wont work on production
+    }
+    
+    [Test]
+    public void GetAction_ShouldMoveUpWhenBallIsAbove()
+    {
+        // given
+        SimpleBot bot = MockSimpleBot.Get();
+        double paddlePosition = 0.0f;
+        Vector2 ballPosition = new Vector2(0.0f, -100.0f);
+        Vector2 ballVelocity = Vector2.Zero;
+
+        // when
+        Action action = bot.GetAction(paddlePosition: 0.0f, ballPosition: ballPosition, ballVelocity: ballVelocity);
+
+        // then
+        Debug.Assert(action == Action.Up); // TODO debug asserts wont work on production
+    }
+
+    private class MockSimpleBot : SimpleBot
+    {
+        public static MockSimpleBot Get() => new();
+    }
+}
